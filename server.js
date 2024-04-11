b45c68b68d083480ab476588a2e0412304567dd3
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-11 21:51:22 +0900

    サーバーを一通り用意

--- server.js
+++ server.js
@@ -5,3 +5,32 @@ const jwt = require('jsonwebtoken');
 // サーバー準備
 const app = express();
 const  PORT = 3000;
+
+// とりあえずjsonを使うのを宣言する必要があるので
+app.use(express.json());
+
+app.listen(PORT,console.log("server running"))
+
+// 登録
+app.post("/register",(req,res) => {
+    // 登録情報
+    const payload = {
+        username:req.body.username,
+        email:req.body.email
+    }
+
+
+    const token = jwt.sign(
+        payload,
+        config.jwt.secret,
+        config.jwt.options
+    )
+
+    const body = {
+        username:req.body.username,
+        email:req.body.email,
+        token:token
+    }
+
+    res.status(200).json(body)
+})
