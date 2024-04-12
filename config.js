8aae050fc32a77d523c21ede5aaa325481f29be0
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-12 11:43:43 +0900

    .envを使うように変更

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 module.exports = {
     jwt:{
-        secret:"SECRET-KKY", //本来なら長いランダムな文字列を使う
+        secret:process.env.SECRETKEY, 
         options:{
             algorithm:"HS256",
             expiresIn:"1d", //トークンの有効期限
